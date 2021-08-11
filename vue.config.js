afe8413b3426f0022fa574bf60832f75e5b487d4
chenkun <chenkun@anmaicloud.com>
2021-08-11 10:16:15 +0800

    修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,24 @@
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                'vue$': 'vue/dist/vue.esm.js'
-            }
-        }
+  // 基本路径
+  publicPath: "./",
+  // html输入路径
+  indexPath: "index.html",
+  // 输出目录
+  outputDir: "dist",
+  // 静态资源目录
+  assetsDir: "",
+  // 生成环境
+  devServer: {
+    open: true,
+    host: "0.0.0.0",
+    port: 10501,
+  },
+  // Webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
     }
+  },
 }
