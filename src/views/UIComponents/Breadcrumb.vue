bbb8b78ef5220d53f4399ffe48456bdfe8310e66
chenkun <chenkun@anmaicloud.com>
2021-08-26 15:58:04 +0800

    面包屑

--- src/views/UIComponents/Breadcrumb.vue
+++ src/views/UIComponents/Breadcrumb.vue
@@ -3,75 +3,78 @@
   <div>
     <!-- AntDesignVue -->
     <a-card class="card-margin" title="AntDesignVue 面包屑">
-      <!-- 正常面包屑 -->
-      <div class="card-item">
-        <h3>正常面包屑</h3>
-        <div class="radio-box">
-          <div class="string-set">
-            <span style="margin-right: 5px">设置面包屑分隔符</span>
-            <a-input style="width: 100px" v-model="breadcrumbSeparator" />
-          </div>
-        </div>
-        <div>
-          <a-breadcrumb :separator="breadcrumbSeparator">
-            <a-breadcrumb-item>
-              <router-link to="/"><a-icon type="home" /></router-link>
-            </a-breadcrumb-item>
-            <a-breadcrumb-item>
-              <a-icon type="inbox" />
-              <router-link to="/antDesignVue/basics">基础组件</router-link>
-            </a-breadcrumb-item>
-            <a-breadcrumb-item>
-              <span>按钮</span>
-            </a-breadcrumb-item>
-          </a-breadcrumb>
+      <div class="radio-box">
+        <div class="string-set">
+          <span style="margin-right: 5px">设置分隔符</span>
+          <a-input style="width: 100px" v-model="breadcrumbSeparator" />
         </div>
       </div>
-      <!-- 下拉菜单面包屑 -->
-      <div class="card-item">
-        <h3>下拉菜单面包屑</h3>
-        <div class="radio-box">
-          <div class="string-set">
-            <span style="margin-right: 5px">面包屑分隔符</span>
-            <a-input style="width: 100px" v-model="breadcrumbSeparator" />
-          </div>
-        </div>
-        <div>
-          <a-breadcrumb :separator="breadcrumbSeparator">
-            <a-breadcrumb-item>
-              <router-link to="/">主页</router-link>
-            </a-breadcrumb-item>
-            <a-breadcrumb-item>
-              <router-link to="/antDesignVue/basics">基础组件</router-link>
-              <a-menu slot="overlay">
-                <a-menu-item>
-                  <router-link to="/antDesignVue/basics">
-                    基础组件
-                  </router-link>
-                </a-menu-item>
-                <a-menu-item>
-                  <router-link to="/antDesignVue/navigation">
-                    导航组件
-                  </router-link>
-                </a-menu-item>
-                <a-menu-item>
-                  <router-link to="/antDesignVue/form">表单组件</router-link>
-                </a-menu-item>
-              </a-menu>
-            </a-breadcrumb-item>
-            <a-breadcrumb-item>
-              <span>按钮</span>
-            </a-breadcrumb-item>
-          </a-breadcrumb>
-        </div>
+      <div>
+        <a-breadcrumb :separator="breadcrumbSeparator">
+          <a-breadcrumb-item>
+            <router-link to="/"><a-icon type="home" />主页</router-link>
+          </a-breadcrumb-item>
+          <a-breadcrumb-item>
+            <router-link to="/UIComponents/button">按钮</router-link>
+          </a-breadcrumb-item>
+          <a-breadcrumb-item>
+            <span>按钮文档</span>
+          </a-breadcrumb-item>
+        </a-breadcrumb>
       </div>
     </a-card>
     <!-- ElementUI -->
-    <a-card class="card-margin" title="ElementUI 面包屑"></a-card>
+    <a-card class="card-margin" title="ElementUI 面包屑">
+      <div class="radio-box">
+        <div class="string-set">
+          <span style="margin-right: 5px">设置分隔符</span>
+          <a-input style="width: 100px" v-model="breadcrumbSeparatorE" />
+        </div>
+      </div>
+      <div>
+        <el-breadcrumb :separator="breadcrumbSeparatorE">
+          <el-breadcrumb-item :to="{ path: '/' }">主页</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: '/UIComponents/button' }">
+            按钮
+          </el-breadcrumb-item>
+          <el-breadcrumb-item>按钮文档</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+    </a-card>
     <!-- AtUI -->
-    <a-card class="card-margin" title="AtUI 面包屑"></a-card>
+    <a-card class="card-margin" title="AtUI 面包屑">
+      <div class="radio-box">
+        <div class="string-set">
+          <span style="margin-right: 5px">设置分隔符</span>
+          <a-input style="width: 100px" v-model="breadcrumbSeparatorA" />
+        </div>
+      </div>
+      <div>
+        <at-breadcrumb :separator="breadcrumbSeparatorA">
+          <at-breadcrumb-item to="/">主页</at-breadcrumb-item>
+          <at-breadcrumb-item to="/UIComponents/button">
+            按钮
+          </at-breadcrumb-item>
+          <at-breadcrumb-item>按钮文档</at-breadcrumb-item>
+        </at-breadcrumb>
+      </div>
+    </a-card>
     <!-- ViewUI -->
-    <a-card class="card-margin" title="ViewUI 面包屑"></a-card>
+    <a-card class="card-margin" title="ViewUI 面包屑">
+      <div class="radio-box">
+        <div class="string-set">
+          <span style="margin-right: 5px">设置分隔符</span>
+          <a-input style="width: 100px" v-model="breadcrumbSeparatorV" />
+        </div>
+      </div>
+      <div>
+        <Breadcrumb :separator="breadcrumbSeparatorV">
+          <BreadcrumbItem to="/">主页</BreadcrumbItem>
+          <BreadcrumbItem to="/UIComponents/button">按钮</BreadcrumbItem>
+          <BreadcrumbItem>按钮文档</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+    </a-card>
   </div>
 </template>
 
@@ -82,8 +85,11 @@ export default {
       // AntDesignVue
       breadcrumbSeparator: "/",
       // ElementUI
+      breadcrumbSeparatorE: "/",
       // AtUI
+      breadcrumbSeparatorA: "/",
       // ViewUI
+      breadcrumbSeparatorV: "/",
     };
   },
   methods: {},
