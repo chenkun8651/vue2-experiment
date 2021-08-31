2cd21df9b8f9f7309df0114c5b7bb060685d0af9
chenkun <chenkun@anmaicloud.com>
2021-08-31 10:30:56 +0800

    分页

--- src/views/UIComponents/Pagination.vue
+++ src/views/UIComponents/Pagination.vue
@@ -4,24 +4,25 @@
     <!-- AntDesignVue -->
     <a-card class="card-margin" title="AntDesignVue 分页">
       <div class="radio-box">
-        <a-radio-group v-model="paginationSize">
-          <a-radio-button value="">默认</a-radio-button>
-          <a-radio-button value="small">小型</a-radio-button>
-        </a-radio-group>
-        <a-radio-group v-model="paginationSimple">
-          <a-radio-button :value="false">复杂分页</a-radio-button>
-          <a-radio-button :value="true">简洁分页</a-radio-button>
-        </a-radio-group>
-        <a-radio-group v-model="paginationDisabled">
-          <a-radio-button :value="false">启用</a-radio-button>
+        <a-radio-group v-model="AntDesignVue.paginationDisabled">
           <a-radio-button :value="true">禁用</a-radio-button>
+          <a-radio-button :value="false">启用</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="AntDesignVue.paginationSimple">
+          <a-radio-button :value="true">简洁分页</a-radio-button>
+          <a-radio-button :value="false">复杂分页</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="AntDesignVue.paginationSize">
+          <a-radio-button :value="null">default</a-radio-button>
+          <a-radio-button :value="'small'">small</a-radio-button>
         </a-radio-group>
       </div>
       <div>
         <a-pagination
-          :size="paginationSize"
-          :simple="paginationSimple"
-          :disabled="paginationDisabled"
+          style="width: 750px"
+          :disabled="AntDesignVue.paginationDisabled"
+          :simple="AntDesignVue.paginationSimple"
+          :size="AntDesignVue.paginationSize"
           :default-current="3"
           :default-page-size="20"
           :page-size-options="['10', '20', '30', '40', '50']"
@@ -35,23 +36,94 @@
     </a-card>
     <!-- ElementUI -->
     <a-card class="card-margin" title="ElementUI 分页">
+      <div class="radio-box">
+        <a-radio-group v-model="ElementUI.paginationDisabled">
+          <a-radio-button :value="true">禁用</a-radio-button>
+          <a-radio-button :value="false">启用</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ElementUI.paginationBackground">
+          <a-radio-button :value="true">显示按钮背景色</a-radio-button>
+          <a-radio-button :value="false">隐藏按钮背景色</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ElementUI.paginationSmall">
+          <a-radio-button :value="false">default</a-radio-button>
+          <a-radio-button :value="true">small</a-radio-button>
+        </a-radio-group>
+      </div>
       <div>
         <el-pagination
-          :background="background"
-          :small="small"
-          :disabled="disabled"
+          style="width: 650px"
+          :disabled="ElementUI.paginationDisabled"
+          :background="ElementUI.paginationBackground"
+          :small="ElementUI.paginationSmall"
+          :current-page="3"
           :page-size="20"
           :page-sizes="[10, 20, 30, 40, 50]"
-          layout="prev, pager, next"
-          :total="50"
+          :total="500"
+          :layout="'total, sizes, prev, pager, next, jumper'"
         >
         </el-pagination>
       </div>
     </a-card>
     <!-- AtUI -->
-    <a-card class="card-margin" title="AtUI 分页"></a-card>
+    <a-card class="card-margin" title="AtUI 分页">
+      <div class="radio-box">
+        <a-radio-group v-model="AtUI.paginationSimple">
+          <a-radio-button :value="true">简洁分页</a-radio-button>
+          <a-radio-button :value="false">复杂分页</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="AtUI.paginationSize">
+          <a-radio-button :value="null">default</a-radio-button>
+          <a-radio-button :value="'small'">small</a-radio-button>
+        </a-radio-group>
+      </div>
+      <div>
+        <at-pagination
+          :simple="AtUI.paginationSimple"
+          :size="AtUI.paginationSize"
+          :current="3"
+          :page-size="20"
+          :page-size-opts="[10, 20, 30, 40, 50]"
+          :total="500"
+          :show-total="true"
+          :show-sizer="true"
+          :show-quickjump="true"
+        >
+        </at-pagination>
+      </div>
+    </a-card>
     <!-- ViewUI -->
-    <a-card class="card-margin" title="ViewUI 分页"></a-card>
+    <a-card class="card-margin" title="ViewUI 分页">
+      <div class="radio-box">
+        <a-radio-group v-model="ViewUI.paginationDisabled">
+          <a-radio-button :value="true">禁用</a-radio-button>
+          <a-radio-button :value="false">启用</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ViewUI.paginationSimple">
+          <a-radio-button :value="true">简洁分页</a-radio-button>
+          <a-radio-button :value="false">复杂分页</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ViewUI.paginationSize">
+          <a-radio-button :value="null">default</a-radio-button>
+          <a-radio-button :value="'small'">small</a-radio-button>
+        </a-radio-group>
+      </div>
+      <div>
+        <Page
+          :disabled="ViewUI.paginationDisabled"
+          :simple="ViewUI.paginationSimple"
+          :size="ViewUI.paginationSize"
+          :current="3"
+          :page-size="20"
+          :page-size-opts="[10, 20, 30, 40, 50]"
+          :total="500"
+          :show-total="true"
+          :show-sizer="true"
+          :show-elevator="true"
+        >
+        </Page>
+      </div>
+    </a-card>
   </div>
 </template>
 
@@ -59,13 +131,25 @@
 export default {
   data() {
     return {
-      // AntDesignVue
-      paginationSize: "",
-      paginationSimple: false,
-      paginationDisabled: false,
-      // ElementUI
-      // AtUI
-      // ViewUI
+      AntDesignVue: {
+        paginationDisabled: false,
+        paginationSimple: false,
+        paginationSize: null,
+      },
+      ElementUI: {
+        paginationDisabled: false,
+        paginationBackground: true,
+        paginationSmall: false,
+      },
+      AtUI: {
+        paginationSimple: false,
+        paginationSize: null,
+      },
+      ViewUI: {
+        paginationDisabled: false,
+        paginationSimple: false,
+        paginationSize: null,
+      },
     };
   },
   methods: {},
