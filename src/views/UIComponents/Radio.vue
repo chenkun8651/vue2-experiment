f9e2bbafba7786f60b0b0632729d0d1bceb5447b
chenkun <chenkun@anmaicloud.com>
2021-08-31 14:17:48 +0800

    单选框

--- src/views/UIComponents/Radio.vue
+++ src/views/UIComponents/Radio.vue
@@ -3,54 +3,148 @@
   <div>
     <!-- AntDesignVue -->
     <a-card class="card-margin" title="AntDesignVue 单选框">
-      <!-- 标准单选框 -->
-      <div class="card-item">
-        <h3>标准单选框</h3>
-        <div class="radio-box">
-          <a-radio-group v-model="radioDisabled">
-            <a-radio-button :value="true">禁用</a-radio-button>
-            <a-radio-button :value="false">启用</a-radio-button>
-          </a-radio-group>
-        </div>
-        <div>
-          <a-radio-group :options="radioOptions" :disabled="radioDisabled">
-          </a-radio-group>
-        </div>
+      <div class="radio-box">
+        <a-radio-group v-model="AntDesignVue.radioDisabled">
+          <a-radio-button :value="true">禁用</a-radio-button>
+          <a-radio-button :value="false">启用</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="AntDesignVue.radioSize">
+          <a-radio-button :value="'large'">large</a-radio-button>
+          <a-radio-button :value="'default'">default</a-radio-button>
+          <a-radio-button :value="'small'">small</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="AntDesignVue.radioButtonStyle">
+          <a-radio-button :value="'outline'">outline</a-radio-button>
+          <a-radio-button :value="'solid'">solid</a-radio-button>
+        </a-radio-group>
       </div>
-      <!-- 按钮单选框 -->
-      <div class="card-item">
-        <h3>按钮单选框（部分属性继承标准单选框）</h3>
-        <div class="radio-box">
-          <a-radio-group v-model="radioSize">
-            <a-radio-button value="large">large</a-radio-button>
-            <a-radio-button value="default">default</a-radio-button>
-            <a-radio-button value="small">small</a-radio-button>
-          </a-radio-group>
-          <a-radio-group v-model="radioButtonStyle">
-            <a-radio-button value="outline">outline</a-radio-button>
-            <a-radio-button value="solid">solid</a-radio-button>
-          </a-radio-group>
-        </div>
-        <div>
-          <a-radio-group
-            :disabled="radioDisabled"
-            :size="radioSize"
-            :button-style="radioButtonStyle"
+      <div>
+        <a-radio-group
+          v-model="radioValue"
+          :options="radioOptions"
+          :disabled="AntDesignVue.radioDisabled"
+        >
+        </a-radio-group>
+        <a-radio-group
+          v-model="radioValue"
+          :disabled="AntDesignVue.radioDisabled"
+          :button-style="AntDesignVue.radioButtonStyle"
+          :size="AntDesignVue.radioSize"
+        >
+          <a-radio-button
+            v-for="(item, index) in radioOptions"
+            :key="index"
+            :value="item.value"
           >
-            <a-radio-button value="1">单选项一</a-radio-button>
-            <a-radio-button value="2">单选项二</a-radio-button>
-            <a-radio-button value="3">单选项三</a-radio-button>
-            <a-radio-button value="4">单选项四</a-radio-button>
-          </a-radio-group>
-        </div>
+            {{ item.label }}
+          </a-radio-button>
+        </a-radio-group>
       </div>
     </a-card>
     <!-- ElementUI -->
-    <a-card class="card-margin" title="ElementUI 单选框"></a-card>
+    <a-card class="card-margin" title="ElementUI 单选框">
+      <div class="radio-box">
+        <a-radio-group v-model="ElementUI.radioDisabled">
+          <a-radio-button :value="true">禁用</a-radio-button>
+          <a-radio-button :value="false">启用</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ElementUI.radioSize">
+          <a-radio-button :value="'medium'">medium</a-radio-button>
+          <a-radio-button :value="'small'">small</a-radio-button>
+          <a-radio-button :value="'mini'">mini</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ElementUI.radioBorder">
+          <a-radio-button :value="true">显示边框</a-radio-button>
+          <a-radio-button :value="false">隐藏边框</a-radio-button>
+        </a-radio-group>
+      </div>
+      <div>
+        <el-radio-group
+          v-model="radioValue"
+          :disabled="ElementUI.radioDisabled"
+        >
+          <el-radio
+            v-for="(item, index) in radioOptions"
+            :key="index"
+            :label="item.value"
+            :border="ElementUI.radioBorder"
+            :size="ElementUI.radioSize"
+          >
+            {{ item.label }}
+          </el-radio>
+        </el-radio-group>
+      </div>
+    </a-card>
     <!-- AtUI -->
-    <a-card class="card-margin" title="AtUI 单选框"></a-card>
+    <a-card class="card-margin" title="AtUI 单选框">
+      <div class="radio-box">
+        <a-radio-group v-model="AtUI.radioDisabled">
+          <a-radio-button :value="true">禁用</a-radio-button>
+          <a-radio-button :value="false">启用</a-radio-button>
+        </a-radio-group>
+      </div>
+      <div>
+        <at-radio-group v-model="radioValue">
+          <at-radio
+            v-for="(item, index) in radioOptions"
+            :key="index"
+            :label="item.value"
+            :disabled="AtUI.radioDisabled"
+          >
+            {{ item.label }}
+          </at-radio>
+        </at-radio-group>
+      </div>
+    </a-card>
     <!-- ViewUI -->
-    <a-card class="card-margin" title="ViewUI 单选框"></a-card>
+    <a-card class="card-margin" title="ViewUI 单选框">
+      <div class="radio-box">
+        <a-radio-group v-model="ViewUI.radioDisabled">
+          <a-radio-button :value="true">禁用</a-radio-button>
+          <a-radio-button :value="false">启用</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ViewUI.radioSize">
+          <a-radio-button :value="'large'">large</a-radio-button>
+          <a-radio-button :value="'default'">default</a-radio-button>
+          <a-radio-button :value="'small'">small</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ViewUI.radioBorder">
+          <a-radio-button :value="true">显示边框</a-radio-button>
+          <a-radio-button :value="false">隐藏边框</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ViewUI.radioType">
+          <a-radio-button :value="null">单选框</a-radio-button>
+          <a-radio-button :value="'button'">单选按钮</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ViewUI.radioVertical">
+          <a-radio-button :value="true">垂直方向</a-radio-button>
+          <a-radio-button :value="false">水平方向</a-radio-button>
+        </a-radio-group>
+        <a-radio-group v-model="ViewUI.radioButtonStyle">
+          <a-radio-button :value="'default'">default</a-radio-button>
+          <a-radio-button :value="'solid'">solid</a-radio-button>
+        </a-radio-group>
+      </div>
+      <div>
+        <RadioGroup
+          v-model="radioValue"
+          :type="ViewUI.radioType"
+          :button-style="ViewUI.radioButtonStyle"
+          :vertical="ViewUI.radioVertical"
+        >
+          <Radio
+            v-for="(item, index) in radioOptions"
+            :key="index"
+            :label="item.value"
+            :disabled="ViewUI.radioDisabled"
+            :border="ViewUI.radioBorder"
+            :size="ViewUI.radioSize"
+          >
+            {{ item.label }}
+          </Radio>
+        </RadioGroup>
+      </div>
+    </a-card>
   </div>
 </template>
 
@@ -59,19 +153,33 @@ const radioOptions = [
   { label: "单选项一", value: 1 },
   { label: "单选项二", value: 2 },
   { label: "单选项三", value: 3 },
-  { label: "单选项四", value: 4 },
 ];
 export default {
   data() {
     return {
-      // AntDesignVue
+      radioValue: 1,
       radioOptions: radioOptions,
-      radioSize: "default",
-      radioDisabled: false,
-      radioButtonStyle: "outline",
-      // ElementUI
-      // AtUI
-      // ViewUI
+      AntDesignVue: {
+        radioDisabled: false,
+        radioSize: "default",
+        radioButtonStyle: "outline",
+      },
+      ElementUI: {
+        radioDisabled: false,
+        radioSize: "medium",
+        radioBorder: false,
+      },
+      AtUI: {
+        radioDisabled: false,
+      },
+      ViewUI: {
+        radioDisabled: false,
+        radioSize: "default",
+        radioBorder: false,
+        radioType: null,
+        radioButtonStyle: "default",
+        radioVertical: false,
+      },
     };
   },
   methods: {},
